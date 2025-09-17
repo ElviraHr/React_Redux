59ce40732875829cd5d035d196f02727606c8830
elvira <grebenukelvira77@gmail.com>
2025-09-17 21:58:53 +0200

    add onMinus, onDivide, onMultiple  redux action& reducer

--- src/components/Counter/Counter.tsx
+++ src/components/Counter/Counter.tsx
@@ -1,5 +1,6 @@
-import { useAppDispatch, useAppSelector} from "store/hooks"
-import { counterSliceActions, 
+import { useAppDispatch, useAppSelector } from "store/hooks"
+import {
+  counterSliceActions,
   counterSliceSelectors,
 } from "store/redux/counter/counterSlice"
 
@@ -7,24 +8,37 @@ import Button from "components/Button/Button"
 
 import { CounterWrapper, Count, ButtonControl } from "./styles"
 
-
 function Counter() {
-
   //хук юз апп диспатч не принимает в себя аргументы, он просто возвращает нам функцию диспатч,
   //  которая передает акстьео нв редюс стор. Потом на переданный экшен запускается нужный нам reducer
-const dispatch = useAppDispatch();
+  const dispatch = useAppDispatch()
 
-//получаем значения из Redux store, затем передаем значения в нужные места в JSX 
-// и т.о. подписываемся на изменения в redux store
-const count = useAppSelector(counterSliceSelectors.count)
+  //получаем значения из Redux store, затем передаем значения в нужные места в JSX
+  // и т.о. подписываемся на изменения в redux store
+  const count = useAppSelector(counterSliceSelectors.count)
   const onPlus = () => {
-    dispatch(counterSliceActions.plus())
-  };
-const onMinus = () => {};
+    //counterSliceActions.plus() - action creator - возаращает нам Экшен (это объект, состоящий из двух пар ключ/значение:
+    // 1. type - cтрока, тип экшена, ко которому у на вызывается редьюсер. Например "VOUNTER/plus")
+    //2. payload - неопределено. Может содержать данные которые мы можем передать в редьюсер
 
+    dispatch(counterSliceActions.plus())
+  }
+  const onMinus = () => {
+    dispatch(counterSliceActions.minus()) // counterSliceActions.minus() = actionCreator
+  }
+
+  const onMultiply = () => {
+    dispatch(counterSliceActions.multiply(3))
+  }
+  const onDivide = () => {
+    dispatch(counterSliceActions.divide(5))
+  }
 
   return (
     <CounterWrapper>
+      <ButtonControl>
+        <Button name="/" onClick={onDivide} />
+      </ButtonControl>
       <ButtonControl>
         <Button name="-" onClick={onMinus} />
       </ButtonControl>
@@ -32,8 +46,11 @@ const onMinus = () => {};
       <ButtonControl>
         <Button name="+" onClick={onPlus} />
       </ButtonControl>
+      <ButtonControl>
+        <Button name="*" onClick={onMultiply} />
+      </ButtonControl>
     </CounterWrapper>
   )
 }
 
-export default Counter;
+export default Counter
--- src/store/redux/counter/counterSlice.ts
+++ src/store/redux/counter/counterSlice.ts
@@ -1,3 +1,4 @@
+import { PayloadAction } from "@reduxjs/toolkit"
 import { createAppSlice } from "store/createAppSlice"
 import { CounterSliceSate } from "./types"
 
@@ -5,26 +6,41 @@ const counterInitialState = {
   count: 0,
 }
 export const counterSlice = createAppSlice({
-  // name - это имя для slice, имя используется для нахождения событий слайса в redux devtools
+  // name - это имя для slice, имя используется для нахождения событий слайса в redux devtools и создании type в action
   name: "COUNTER",
   // initialState - state, в котором мы прописываем значения по умолчанию
   initialState: counterInitialState,
-  // reducers - это функция, которая возвращает обьект содержащий функции, которые будут изменять стейт
+  // reducers - это функция, которая возвращает обьект содержащий функции-редьюсеры, которые в свою очередь будут изменять стейт
   reducers: create => ({
     plus: create.reducer((state: CounterSliceSate) => {
       state.count = state.count + 1
+    }),
+    minus: create.reducer((state: CounterSliceSate) => {
+      state.count = state.count - 1
+    }),
+
+    multiply: create.reducer(
+      (state: CounterSliceSate, action: PayloadAction<number>) => {
+        //через дженерик передаем тип
+        //payload - данные которые мы хотим передать из компонента в Редьюсер
+        console.log(action.payload)
+        state.count = Number((state.count * action.payload).toFixed(2)) //т.к. toFixed возвращает строку, а нам надо передать обратно число, приводим к типу Number
+      },
+    ),
+    divide: create.reducer((state: CounterSliceSate, action: PayloadAction<number>) => {
+      state.count = Number((state.count / action.payload).toFixed(2))
     }),
   }),
   //selectors - прописываем какие именно данные мы хотим отдать копонентам (необязательные данные:) (обязательные три выше)
   selectors: {
     count: (state: CounterSliceSate) => {
-        return state.count
-    }
-  }
+      return state.count
+    },
+  },
 })
 
 //counterSlice  сам создает actions для каждого отдельного reduser'a
-export const counterSliceActions = counterSlice.actions;
+export const counterSliceActions = counterSlice.actions
 
 //selectors - данные, которые мы будем отдавать компонентам, то есть позволять компонентам подписываться на redux store
-export const counterSliceSelectors = counterSlice.selectors;
+export const counterSliceSelectors = counterSlice.selectors
